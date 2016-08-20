331d3e6e217c9ddc96875130e5aa0e64f1ea8777
Antonio Marques Jr <antonioricardojr@gmail.com>
2016-08-20 14:34:01 -0300

    Adicionando template inicial

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# vereador
+# O que faz um vereador?
+
+## Hackfest 7, Campina Grande-PB
+## 20 de agosto de 2016
+
---
+++ js/MainController.js
@@ -0,0 +1,6 @@
+var app = angular.module( 'app' );
+
+app.controller( 'MainController', [ '$scope', function ( $scope ) {
+
+    $scope.title = "O que faz o vereador?";
+} ] );
