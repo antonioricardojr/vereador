bf6790e9ddf9c4b07b4b71cbe5cd5597fa850a97
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2016-09-04 11:18:05 -0300

    Fatora o código em dois arquivos/camadas: lógica e acesso a dados

---
+++ R/data_access.R
@@ -0,0 +1,69 @@
+#' 
+#' Funções que acessam o BD. Tudo que tiver SQL fica aqui. 
+#'
+
+start_camaraDB <- function(port = 5432, host = "localhost") {
+  library(RPostgreSQL)
+  library(dplyr)
+  postgres_user <- Sys.getenv("POSTGRES_USER")
+  postgres_pass <- Sys.getenv("POSTGRES_PASS")
+  src_postgres(
+    dbname = "camara_db",
+    user = postgres_user,
+    password = postgres_pass,
+    port = port,
+    host = host
+  )
+}
+
+get_ementasraw = function(db){
+  ementas_raw <- tbl(db, sql("SELECT * FROM ementas")) %>%
+    filter(published_date >= "2009-01-01") %>%
+    mutate(published_month = date_trunc('month', published_date)) %>%
+    return()
+}
+
+get_vereadores_raw = function(db, id = NA, ano_eleicao){
+  if (is.na(id)) {
+    tbl(db, sql(paste0("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE' and ano_eleicao = ", ano_eleicao))) %>%
+      return()
+  } else{
+    tbl(db, sql(paste0("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE' and sequencial_candidato = ",
+                       id,
+                       " and ano_eleicao = ",
+                       ano_eleicao))) %>%
+      return()
+  }
+}
+
+get_ementas_por_vereador_raw = function(db, id_candidato, ano) {
+  #' Junção de vereador com ementas
+  tbl(db,
+      sql(
+        paste0(
+          "SELECT v.sequencial_candidato, v.nome_urna_candidato, e.*
+          FROM consulta_cand v, ementas e, map_ementa_candidato map
+          WHERE map.sequencial_candidato = v.sequencial_candidato and
+          map.ementa_id = e.ementa_id and
+          map.published_date > '2013-01-01' and
+          v.ano_eleicao = ", ano, " and v.sequencial_candidato = ", id_candidato
+        )
+        )) %>%
+    return()
+}
+
+get_propostas_todos_vereador_raw = function(db, ano) {
+  #' Junção de vereador com ementas, exibir todas as propostas
+  tbl(db,
+      sql(
+        paste0(
+          "SELECT v.sequencial_candidato, v.nome_urna_candidato, e.*
+          FROM consulta_cand v, ementas e, map_ementa_candidato map
+          WHERE map.sequencial_candidato = v.sequencial_candidato and
+          map.ementa_id = e.ementa_id and
+          map.published_date > '2013-01-01' and
+          v.ano_eleicao = ", ano
+        )
+        ))  %>%
+    return()
+}
--- R/vereadores_logic.R
+++ R/vereadores_logic.R
@@ -1,24 +1,4 @@
-
-start_camaraDB <- function(port = 5432, host = "localhost") {
-    library(RPostgreSQL)
-    library(dplyr)
-    postgres_user <- Sys.getenv("POSTGRES_USER")
-    postgres_pass <- Sys.getenv("POSTGRES_PASS")
-    src_postgres(
-        dbname = "camara_db",
-        user = postgres_user,
-        password = postgres_pass,
-        port = port,
-        host = host
-    )
-}
-
-get_ementasraw = function(db){
-    ementas_raw <- tbl(db, sql("SELECT * FROM ementas")) %>%
-        filter(published_date >= "2009-01-01") %>%
-        mutate(published_month = date_trunc('month', published_date)) %>%
-        return()
-}
+source("./data_access.R")
 
 get_ementas_all = function(db) {
     #' Opções de group_by:
@@ -66,26 +46,6 @@ get_ementas_all = function(db) {
     return(ementas)
 }
 
-get_vereadores_raw = function(db, id = NA, ano_eleicao = NA){
-    if (is.na(id) & is.na(ano_eleicao)) {
-        tbl(db, sql("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE'")) %>%
-            return()
-    }
-    if (is.na(ano_eleicao)) {
-        tbl(db, sql(paste0("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE' and sequencial_candidato = ", id))) %>%
-            return()
-    }
-    if (is.na(id)) {
-        tbl(db, sql(paste0("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE' and ano_eleicao = ", ano_eleicao))) %>%
-            return()
-    } else{
-        tbl(db, sql(paste0("SELECT * FROM consulta_cand where descricao_ue = 'CAMPINA GRANDE' and sequencial_candidato = ",
-                           id,
-                           " and ano_eleicao = ",
-                           ano_eleicao))) %>%
-            return()
-    }
-}
 
 get_vereadores = function(db, id = NA, ano_eleicao = 2012){
     vereadores_lista <- get_vereadores_raw(db, id, ano_eleicao) %>%
@@ -93,38 +53,6 @@ get_vereadores = function(db, id = NA, ano_eleicao = 2012){
         collect()
 
     return(vereadores_lista)
-}
-
-get_ementas_por_vereador_raw = function(db, id_candidato, ano) {
-#' Junção de vereador com ementas
-    tbl(db,
-        sql(
-            paste0(
-                "SELECT v.sequencial_candidato, v.nome_urna_candidato, e.*
-                  FROM consulta_cand v, ementas e, map_ementa_candidato map
-                  WHERE map.sequencial_candidato = v.sequencial_candidato and
-                        map.ementa_id = e.ementa_id and
-                        map.published_date > '2013-01-01' and
-                        v.ano_eleicao = ", ano, " and v.sequencial_candidato = ", id_candidato
-            )
-        )) %>%
-        return()
-}
-
-get_propostas_todos_vereador_raw = function(db, ano) {
-#' Junção de vereador com ementas, exibir todas as propostas
-    tbl(db,
-        sql(
-            paste0(
-            "SELECT v.sequencial_candidato, v.nome_urna_candidato, e.*
-            FROM consulta_cand v, ementas e, map_ementa_candidato map
-            WHERE map.sequencial_candidato = v.sequencial_candidato and
-                map.ementa_id = e.ementa_id and
-                map.published_date > '2013-01-01' and
-                v.ano_eleicao = ", ano
-            )
-        ))  %>%
-    return()
 }
 
 get_ementas_por_vereador = function(db, id_candidato = NA, ano) {
